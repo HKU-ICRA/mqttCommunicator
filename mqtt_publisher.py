c0f6731c07ade8cb46b52911badec0c8b1ee37ae
rexlee <rexlee@connect.hku.hk>
2020-01-14 16:03:52 +0800

    Communication with MQTT version 1

---
+++ mqtt_publisher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Pose
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+
+#MQTT info
+hostname = "mqtt.eclipse.org" #Sandbox broker
+port = 1883 #Default port for unencrypted MQTT
+topic = "HeraKules_robot001/test"
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() +
+            " Get Pose info:\n" + str(data) + "\nSending to mqtt client.")
+    #payload limited to string, bytearray, int, float or none
+    #For bytearray: elements must be 0 <= x < 256
+    mqttc.publish(topic, payload = str(data), qos = 0, retain = False)
+
+def on_connect(client, userdata, flags, rc):
+    #Successfully connection is '0'
+    rospy.loginfo("Connection result: " + str(rc))
+
+def on_publish(client, userdata, mid):
+    rospy.loginfo("Message sent.")
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        rospy.loginfo("Disconnected unexpectedly")
+
+mqttc = mqtt.Client()
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_disconnect = on_disconnect
+mqttc.connect(hostname, port = port, keepalive = 60, bind_address = "")
+
+def mqttPublisher():
+
+    rospy.init_node('mqtt_publisher', anonymous=True)
+
+    #First create a subscriber to acquire Pose info from
+    #certain ROS topic.
+    rospy.Subscriber('robot_pose', Pose, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    mqttPublisher()
---
+++ mqtt_subscriber.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Pose
+import paho.mqtt.client as mqtt
+
+#MQTT info
+hostname = "mqtt.eclipse.org" #Sandbox broker
+port = 1883 #Default port for unencrypted MQTT
+topic = "HeraKules_robot001/#"
+
+pub = rospy.Publisher('comm_message', Pose, queue_size = 1000)
+
+def on_connect(client, userdata, flags, rc):
+    #Successfully connection is '0'
+    rospy.loginfo("Connection result: " + str(rc))
+    if rc == 0:
+        client.subscribe(topic)
+
+def on_message(client, userdata, message):
+    rospy.loginfo("Received message on " + message.topic +
+            ":\n" + str(message.payload))
+    comm_msg = Pose()
+    raw_msg = str(message.payload).split('\n')
+    comm_msg.position.x = float(raw_msg[1][5:])
+    comm_msg.position.y = float(raw_msg[2][5:])
+    comm_msg.position.z = float(raw_msg[3][5:])
+    comm_msg.orientation.x = float(raw_msg[5][5:])
+    comm_msg.orientation.y = float(raw_msg[6][5:])
+    comm_msg.orientation.z = float(raw_msg[7][5:])
+    comm_msg.orientation.w = float(raw_msg[8][5:])
+    pub.publish(comm_msg)
+
+def on_subscribe(client, userdata, mid):
+    rospy.loginfo("Message received.")
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        rospy.loginfo("Disconnected unexpectedly")
+
+#Initialize client instance
+client = mqtt.Client()
+#Bind events to functions
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_subscribe = on_subscribe
+client.on_disconnect = on_disconnect
+#Connect to the specified broker
+client.connect(hostname, port = port)
+
+def mqttSubscriber():
+    
+    rospy.init_node('mqtt_subsriber', anonymous = True)
+
+    client.subscribe("topic", qos = 0)    
+
+    #Network loop runs in the background to listen to the events
+    while not rospy.is_shutdown():
+        client.loop()
+
+if __name__ == '__main__':
+    try:
+        mqttSubscriber()
+    except rospy.ROSInterruptException:
+        pass
